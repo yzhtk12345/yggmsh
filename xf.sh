72cb2cd6fa0f3d691973b8dbc6367890375cfcbd
yzhtk12345 <59499387+yzhtk12345@users.noreply.github.com>
2022-06-08 15:33:34 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "poiuytre-qwer-asdf-zxcv-lkjhgfdsamnb",
                         "alterId": 0
                     }
                 ],
